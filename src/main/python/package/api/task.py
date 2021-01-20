c125384e83a7d4bb6da9f5fba78b7cd15efb91bd
RiviereGregory <gregory.riviere@laposte.net>
2021-01-20 19:19:58 +0100

    Ajout constante

---
+++ src/main/python/package/api/task.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+import logging
+import  json
+
+logging.basicConfig(level=logging.DEBUG)
+
+TASKS_DIR = os.path.join(Path.home(), ".todo")
+TASKS_FILEPATH = os.path.join(TASKS_DIR, "tasks.json")
