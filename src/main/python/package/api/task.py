16b92b8699354902f1d404b0ec193dc7efc3f1c7
RiviereGregory <gregory.riviere@laposte.net>
2021-01-20 19:34:48 +0100

    Ajout fonction de récupère les taches

--- src/main/python/package/api/task.py
+++ src/main/python/package/api/task.py
@@ -1,9 +1,21 @@
+import json
+import logging
 import os
 from pathlib import Path
-import logging
-import  json
 
 logging.basicConfig(level=logging.DEBUG)
 
 TASKS_DIR = os.path.join(Path.home(), ".todo")
 TASKS_FILEPATH = os.path.join(TASKS_DIR, "tasks.json")
+
+
+def get_tasks():
+    if os.path.exists(TASKS_FILEPATH):
+        with open(TASKS_FILEPATH, "r") as f:
+            return json.load(f)
+
+    return {}
+
+
+if __name__ == '__main__':
+    t = get_tasks()
