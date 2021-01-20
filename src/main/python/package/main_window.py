d806e55b9f4662cf4bedb3268c307c78010952fb
RiviereGregory <gregory.riviere@laposte.net>
2021-01-20 20:19:29 +0100

    Ajout widgets et layout

--- src/main/python/package/main_window.py
+++ src/main/python/package/main_window.py
@@ -1,10 +1,12 @@
 from PySide2 import QtWidgets
 
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setup_ui()
 
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self, ctx):
+        super().__init__()
+        self.ctx = ctx
+        self.setWindowTitle("PyTasks")
+        self.setup_ui()
 
     def setup_ui(self):
         self.create_widgets()
@@ -14,16 +16,26 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setup_connections()
 
     def create_widgets(self):
-        pass
+        self.lw_tasks = QtWidgets.QListWidget()
+        self.btn_add = QtWidgets.QPushButton("Add")
+        self.btn_clean = QtWidgets.QPushButton("Clean")
+        self.btn_quit = QtWidgets.QPushButton("Quit")
 
     def modify_widgets(self):
         pass
 
     def create_layouts(self):
-        pass
+        self.main_layout = QtWidgets.QVBoxLayout(self)
+        self.layout_buttons = QtWidgets.QHBoxLayout()
 
     def add_widgets_to_layouts(self):
-        pass
+        self.main_layout.addWidget(self.lw_tasks)
+        self.main_layout.addLayout(self.layout_buttons)
+
+        self.layout_buttons.addWidget(self.btn_add)
+        self.layout_buttons.addStretch()
+        self.layout_buttons.addWidget(self.btn_clean)
+        self.layout_buttons.addWidget(self.btn_quit)
 
     def setup_connections(self):
         pass
